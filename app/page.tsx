b4919db5683cd55b7b1f47cdff67d9fbb39f7b9c
westmcmillen <westmcmillen@gmail.com>
2023-08-29 11:50:05 -0700

    Modifies className to trigger redeployment

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { ContactForm } from "@/components/contact-form";
 
 export default function Page() {
   return (
-    <div className="relative">
+    <div className="relative w-full">
       <div className="sticky top-0 z-50 flex items-center bg-white px-3 py-4 shadow-md">
         <h3 className="text-sm font-semibold uppercase">Big Feast Rentals</h3>
       </div>
